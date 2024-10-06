bd750ea71af608e0090b55c3975a35d24e53cc44
PokemonOwner <160018170+PokemonOwner@users.noreply.github.com>
2024-10-06 13:55:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "23595172"))
 API_HASH = getenv("API_HASH", "d4c35b7ef112914876fc34eceb4dfe2d")
-BOT_TOKEN = getenv("BOT_TOKEN", "7797834536:AAED4H8rdpBMtgPgl5NmPZc-IlJMlPhilmI")
+BOT_TOKEN = getenv("BOT_TOKEN", "7797834536:AAEyQW_A9w5Yl5b2a0M6HyK2pgzx4kZY6IQ")
 OWNER_ID = int(getenv("OWNER_ID", "7002735087"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://techmonofficial:techmon123@cluster0.ga2uy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_GROUP = int(getenv("LOG_GROUP", "-1002323134166"))
